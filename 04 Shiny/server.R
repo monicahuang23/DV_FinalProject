3e4181329889f90acb1b39a0cfcd4283fae94cc2
Monica Huang <monicahuang@wireless-10-146-5-227.public.utexas.edu>
2015-12-08 10:54:11 -0600

    no message

--- 04 Shiny/server.R
+++ 04 Shiny/server.R
@@ -9,54 +9,86 @@ require(leaflet)
 
 shinyServer(function(input, output) {
 
-# start of plot 2      
-      KPI_bad_attendance <- reactive({input$KPI1})     
-      KPI_average_attendance <- reactive({input$KPI2})
-      rv <- reactiveValues(alpha = 0.50)
-      observeEvent(input$light, { rv$alpha <- 0.50 })
-      observeEvent(input$dark, { rv$alpha <- 0.75 })
-      
-      df2 <- eventReactive(input$clicks2, {data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
-            "select SCHOOL_LEVEL, district_name, kpi,
-            case                                                                                   when kpi < "p1" then \\\'Bad Attendance\\\'
-            when kpi < "p2" then \\\'Average Attendance\\\'
-            else \\\'Great Attendance\\\'
-            end kpi                                                                                from (select school_level, district_name, avg(attendance_rate) as kpi
-            from truancy 
-            where DISTRICT_CODE <= 10
-            group by SCHOOL_LEVEL, DISTRICT_NAME);"
-            ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_mh42375', PASS='orcl_mh42375', 
-                 MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_bad_attendance(), p2=KPI_average_attendance()), verbose = TRUE)))
+      observeEvent(input$clicks1, {
+        if (input$scatter == "all_scatter") {
+          df1 <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query="select x_free_and_reduced, tot_days_possible, tot_days_attended, school_level, (tot_days_attended/tot_days_possible) as Act_Attendance_Rate from (select * from truancy
+      LEFT JOIN k_12_frl
+      ON truancy.SCHOOL_NAME=k_12_frl.SCHOOL_NAME
+      LEFT JOIN enrl_working
+      ON truancy.SCHOOL_NAME=enrl_working.SCHOOL_NAME
+      LEFT JOIN final_grade
+      ON truancy.SCHOOL_NAME=final_grade.SCHOOLNAME)
+    where tot_days_possible > tot_days_attended
+    and x_free_and_reduced is not null"')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))) 
+        }
+
+       
+       if (input$scatter == "elem") {
+        df1 <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query="select x_free_and_reduced, tot_days_possible, tot_days_attended, school_level, (tot_days_attended/tot_days_possible) as Act_Attendance_Rate from (select * from truancy LEFT JOIN k_12_frl ON truancy.SCHOOL_NAME=k_12_frl.SCHOOL_NAME LEFT JOIN enrl_working ON truancy.SCHOOL_NAME=enrl_working.SCHOOL_NAME LEFT JOIN final_grade ON truancy.SCHOOL_NAME=final_grade.SCHOOLNAME) where tot_days_possible > tot_days_attended and x_free_and_reduced is not null and school_level = \\\'Elementary\\\' "')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+       }
+       if (input$scatter == "mid") {
+        df1 <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query="select x_free_and_reduced, tot_days_possible, tot_days_attended, school_level, (tot_days_attended/tot_days_possible) as Act_Attendance_Rate from (select * from truancy LEFT JOIN k_12_frl ON truancy.SCHOOL_NAME=k_12_frl.SCHOOL_NAME LEFT JOIN enrl_working ON truancy.SCHOOL_NAME=enrl_working.SCHOOL_NAME LEFT JOIN final_grade ON truancy.SCHOOL_NAME=final_grade.SCHOOLNAME) where tot_days_possible > tot_days_attended and x_free_and_reduced is not null and school_level = \\\'Middle/Jr. High\\\' "')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+       }
+       if (input$scatter == "high") {
+        df1 <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query="select x_free_and_reduced, tot_days_possible, tot_days_attended, school_level, (tot_days_attended/tot_days_possible) as Act_Attendance_Rate from (select * from truancy LEFT JOIN k_12_frl ON truancy.SCHOOL_NAME=k_12_frl.SCHOOL_NAME LEFT JOIN enrl_working ON truancy.SCHOOL_NAME=enrl_working.SCHOOL_NAME LEFT JOIN final_grade ON truancy.SCHOOL_NAME=final_grade.SCHOOLNAME) where tot_days_possible > tot_days_attended and x_free_and_reduced is not null and school_level = \\\'Senior\\\' "')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE))) 
+       }
+      output$distPlot1 <- renderPlot({             
+        plot1 <- ggplot() + 
+          coord_cartesian() + 
+          scale_x_continuous() +
+          scale_y_continuous() +
+          labs(title=isolate(input$title)) +
+          labs(x="Attendance Rate", y=paste("Free and Reduced Meals")) +
+          layer(data=df1(), 
+                mapping=aes(x=ACT_ATTENDANCE_RATE, y=X_FREE_AND_REDUCED, color = SCHOOL_LEVEL),
+                stat="identity", 
+                stat_params=list(), 
+                geom="point",
+                position=position_jitter(width=0.1, height=0)
+          )  
+        plot1
+        })
+      })
+      observeEvent(input$clicks, {
+            print(as.numeric(input$clicks))
+      })
+  
+      # start of plot 3
+      df3 <- eventReactive(input$clicks3, {data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query="SELECT final_plantype, avg(overall_weighted_growth_grade) as avg_weighted_gg, avg(rank_tot) as avg_rank_tot FROM final_grade WHERE overall_weighted_growth_grade is not null AND rank_tot is not null and final_plantype is not null group by final_plantype order by final_plantype;"')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
       })
       
-      output$distPlot2 <- renderPlot({             
-            plot2 <- ggplot() + 
-                  coord_cartesian() + 
-                  scale_x_discrete() +
-                  scale_y_discrete() +
-                  labs(title=isolate(input$title)) +
-                  labs(x=paste("School Level"), y=paste("District Name")) +
-                  layer(data=df2(), 
-                        mapping=aes(x=SCHOOL_LEVEL, y=DISTRICT_NAME, label=round(KPI)), 
-                        stat="identity", 
-                        stat_params=list(), 
-                        geom="text",
-                        geom_params=list(colour="black"), 
-                        position=position_identity()
-                  ) +
-                  layer(data=df2(), 
-                        mapping=aes(x=SCHOOL_LEVEL, y=DISTRICT_NAME, fill=KPI.1), 
-                        stat="identity", 
-                        stat_params=list(), 
-                        geom="tile",
-                        geom_params=list(alpha=rv$alpha), 
-                        position=position_identity()
-                  )
-           plot2
-      }) 
+      output$distPlot3 <- renderPlot({
+        plot3 <- ggplot() + 
+          coord_cartesian() + 
+          scale_x_discrete() +
+          scale_y_continuous() +
+          labs(title='Overall Weighted Growth Average for Final Plantype') +
+          labs(x="Final Plantype", y=paste("Avg Overall Weighted Growth Grade")) +
+          layer(data=df3(), 
+                mapping=aes(x=as.character(FINAL_PLANTYPE), y=AVG_WEIGHTED_GG, fill=AVG_RANK_TOT),
+                stat="identity", 
+                stat_params=list(), 
+                geom="bar",
+                geom_params=list(), 
+                position=position_identity()
+          ) +
+          theme(axis.text.x = element_text(size  = 10, angle = 45, hjust = 1, vjust = 1))
+        if (input$avg_read) {
+          plot3 <- plot3 + geom_hline(aes(yintercept=6.62699, colour = "red")) +
+            annotate("text", label = "Avg Reading Growth Grade", x = 2, 6.3, size = 4, colour = "red")
+        }
+        if (input$avg_math) {
+          plot3 <- plot3 + geom_hline(aes(yintercept=6.71413, colour = "black")) +
+            annotate("text", label = "Avg Math Growth Grade", x = 2, 7, size = 4, colour = "black")
+        }
+        if (input$avg_write) {
+          plot3 <- plot3 + geom_hline(aes(yintercept=6.69606, colour = "blue")) +
+            annotate("text", label = "Avg Writing Growth Grade", x = 5, 7, size = 4, colour = "blue")
+        }
+        plot3
+      })
       
       observeEvent(input$clicks, {
         print(as.numeric(input$clicks))
       })
-
 })
--- 04 Shiny/ui.R
+++ 04 Shiny/ui.R
@@ -4,11 +4,11 @@ library(shiny)
 
 navbarPage(
   title = "DV_FinalProject",
-  tabPanel(title = "Non-Aggregated Measure Analysis",
+  tabPanel(title = "Scatterplot",
      sidebarPanel(
        textInput(inputId = "title", 
                  label = "Title",
-                 value = "Non-Aggregated Measures"),
+                 value = "Attendance Rate vs FRL"),
        actionButton(inputId = "clicks1",  label = "Show the plot")
      ),
      
@@ -16,69 +16,41 @@ navbarPage(
      )
   ),
   tabPanel(title = "Crosstab",
-     sidebarPanel(
-       actionButton(inputId = "light", label = "Light"),
-       actionButton(inputId = "dark", label = "Dark"),
-       sliderInput("KPI1", "KPI_bad_attendance:", 
-                   min = 1, max = 20,  value = 20),
-       sliderInput("KPI2", "KPI_average_attendance:", 
-                   min = 10, max = 32,  value = 32),
-       textInput(inputId = "title", 
-                 label = "CrossTab Title",
-                 value = "CrossTab"),
-       
-       actionButton(inputId = "clicks2",  label = "Show the plot")
-     ),
-     
-     mainPanel(plotOutput("distPlot2")
-     )
-  ),
-  tabPanel(title = "Scatter Plot",
-     sidebarPanel(
-       checkboxInput(inputId = "avg_read",
-                     label = strong("Show average reading growth grades"),
-                     value = FALSE),
-       
-       checkboxInput(inputId = "avg_math",
-                     label = strong("Show average math growth grades"),
-                     value = FALSE),
-
-       checkboxInput(inputId = "avg_write",
-                     label = strong("Show average writing growth grades"),
-                     value = FALSE),
-       
-       actionButton(inputId = "clicks3",  label = "Show the plot")
-     ),
-     
-     mainPanel(plotOutput("distPlot3")
-     )        
-
-  ),
-  tabPanel(title = "Crosstab",
-    sidebarPanel(
-      actionButton(inputId = "light", label = "Light"),
-      actionButton(inputId = "dark", label = "Dark"),
-      sliderInput("KPI1", "KPI_great:", 
-                  min = 1, max = 1000,  value = 1000),
-      sliderInput("KPI2", "KPI_average:", 
-                  min = 1000, max = 10000,  value = 10000),
-      textInput(inputId = "title", 
-                label = "CrossTab Title",
-                value = "CrossTab"),
+           sidebarPanel(
+             actionButton(inputId = "light", label = "Light"),
+             actionButton(inputId = "dark", label = "Dark"),
+             sliderInput("KPI1", "KPI_bad_attendance:", 
+                         min = 1, max = 20,  value = 20),
+             sliderInput("KPI2", "KPI_average_attendance:", 
+                         min = 10, max = 32,  value = 32),
+             textInput(inputId = "title", 
+                       label = "CrossTab Title",
+                       value = "CrossTab"),
              
-      actionButton(inputId = "clicks4",  label = "Show the plot")
-    ),
+             actionButton(inputId = "clicks2",  label = "Show the plot")
+           ),
            
-    mainPanel(plotOutput("distPlot4")
-    )              
+           mainPanel(plotOutput("distPlot2")
+           )
   ),
   tabPanel(title = "Barchart",
-    sidebarPanel(
+           sidebarPanel(
+             checkboxInput(inputId = "avg_read",
+                           label = strong("Show average reading growth grades"),
+                           value = FALSE),
              
-      actionButton(inputId = "clicks5",  label = "Show the plot")
-    ),
+             checkboxInput(inputId = "avg_math",
+                           label = strong("Show average math growth grades"),
+                           value = FALSE),
+             
+             checkboxInput(inputId = "avg_write",
+                           label = strong("Show average writing growth grades"),
+                           value = FALSE),
+             
+             actionButton(inputId = "clicks3",  label = "Show the plot")
+           ),
            
-    mainPanel(plotOutput("distPlot5")
-    )
+           mainPanel(plotOutput("distPlot3")
+           )      
   )  
 )
